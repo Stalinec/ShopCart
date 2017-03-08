304beffc637844470d5d04e33463c81ad433c84a
Yurii Tereshchuk <stalinec7@gmail.com>
2017-03-08 23:18:42 +0200

    added signup page with cfsr protection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrProtection = csrf();
+router.use(csrProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +16,14 @@ router.get('/', function(req, res, next) {
     }
     res.render('shop/index', { title: 'Shop Cart', products: productChunks });
   });
+});
+
+router.get('/user/signup', function(req, res, next){
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', function(req, res, next){
+  res.redirect('/');
 });
 
 module.exports = router;
