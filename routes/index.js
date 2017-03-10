889c5f61517e1c0d62252c486c52ea42ad7b2b3c
Yuriy Tereshchuk <stalinec7@gmail.com>
2017-03-10 18:22:10 +0200

    added payment service

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var Cart = require('../models/cart');
 
+var Cart = require('../models/cart');
 var Product = require('../models/product');
+var Order = require('../models/order');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find(function(error, docs){
     var productChunks = [];
     var chunkSize = 3;
     for(var i = 0; i < docs.length; i += chunkSize){
       productChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/index', { title: 'Shop Cart', products: productChunks });
+    res.render('shop/index', { title: 'Shop Cart', products: productChunks, successMsg: successMsg, noMessages: !successMsg });
   });
 });
 
@@ -37,6 +39,50 @@ router.get('/shop-cart', function(req, res, next){
   }
   var cart = new Cart(req.session.cart);
   res.render('shop/shop-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/checkout', function(req, res, next){
+  if(!req.session.cart){
+    return res.redirect('/shop-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noErrors: !errMsg});
+})
+
+router.post('/checkout', function(req, res, next){
+  if(!req.session.cart){
+    return res.redirect('/shop-cart');
+  }
+  var cart = new Cart(req.session.cart);
+
+  var stripe = require("stripe")(
+    "sk_test_mjr85jEg0mgORxkcVPeFJAVZ"
+  );
+  stripe.charges.create({
+    amount: cart.totalPrice * 100, // price in cents
+    currency: "usd",
+    source: req.body.stripeToken, // obtained with Stripe.js
+    description: "Test Charge"
+  }, function(err, charge) {
+    if(err){
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
+    }
+    var order = new Order({
+      user: req.user, // passport will place the user in request
+      cart: cart,
+      address: req.body.address,
+      name: req.body.name,
+      paymentId: charge.id
+    });
+    order.save(function(err, result){
+      req.flash('success', 'Successfully bought product');
+      req.session.cart = null;
+      res.redirect('/');
+    });
+
+  });
 });
 
 module.exports = router;
